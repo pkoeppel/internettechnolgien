616cf65995f45bd1d09aa0a47f6f8d45a92b3934
pkoeppel <pkoeppel@gk-software.com>
2023-06-13 11:10:57 +0200

    upload HelloWorldProject

---
+++ source/repos/HelloWorldProject/HelloWorldProject/HelloWorldProject/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorldProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string username = nameEntry.Text;
+            string greeting = "Hello " + username;
+            greetingName.Text = greeting;
+        }
+    }
+}
